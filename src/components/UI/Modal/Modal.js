01ab89cf40f0740a94cee2d0e2954b0db6eb3f33
Anthony Jung <junganthonyy@gmail.com>
2020-05-14 14:28:47 -0700

    Adding Backdrop To Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import classes from './Modal.css';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => (
-  <div 
-    className={classes.Modal}
-    style={{
-      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-      opacity: props.show ? '1' : '0'
-    }}
-  >
-    {props.children}
-  </div>
+  <Aux>
+    <Backdrop show={props.show} clicked={props.modalClosed}/>
+    <div
+      className={classes.Modal}
+      style={{
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1' : '0'
+      }}
+      >
+      {props.children}
+    </div>
+  </Aux>
 );
+/**
+ * Doing inline stylings to handle dynamic display
+ * of the modal. translateY lets it slide in and out.
+ * vh units are relative to the viewport.
+ * opacity handles the actual changing of the visiblity
+ */
 
 export default modal;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -82,6 +82,12 @@ class BurgerBuilder extends Component {
     });
   }
 
+  purchasedCanceledHandler = () => {
+    this.setState({
+      purchasing: false
+    });
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -92,7 +98,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} modalClosed={this.purchasedCanceledHandler}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients}/>

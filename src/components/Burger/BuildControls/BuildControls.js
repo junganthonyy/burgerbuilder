f2efb65467dc710ff1384c0aaf47006b57802571
Anthony Jung <junganthonyy@gmail.com>
2020-05-14 09:56:54 -0700

    Implementing BuildControls to BugerBuilder

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import classes from './buildControls.css';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+// import PropTypes from 'prop-types';
+
+const controls = [
+  {label: 'Salad', type: 'salad'},
+  {label: 'Bacon', type: 'bacon'},
+  {label: 'Cheese', type: 'cheese'},
+  {label: 'Meat', type: 'meat'},
+];
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
-
+    {controls.map((control) => (
+      <BuildControl label={control.label} key={control.label}/>
+    ))}
   </div>
 )
 

11674763605ac939a9d25afe18ce49e4eab6e386
Anthony Jung <junganthonyy@gmail.com>
2020-05-14 17:47:34 -0700

    Adding Optimization To Component Updates

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,28 +2,34 @@ import React from 'react';
 import Button from '../../UI/Button/Button';
 import Aux from '../../../hoc/Aux';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((el) => {
+class OrderSummary extends React.Component {
+  // this could be a functional component
+  componentWillUpdate() {
+    console.log('[OrderSummary] Will Update');
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map((el) => {
+      return (
+        <li key={el}>
+          <span style={{textTransform: 'capitalize'}}>{el}</span>: {this.props.ingredients[el]}
+        </li>
+      )
+    })
+
     return (
-      <li key={el}>
-        <span style={{textTransform: 'capitalize'}}>{el}</span>: {props.ingredients[el]}
-      </li>
-    )
-  })
-  
-  return (
-    <Aux>
+      <Aux>
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>
         {ingredientSummary}
       </ul>
-      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
+      <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
       <p>Continue To Checkout?</p>
-      <Button clicked={props.purchaseCanceled} btnType="Danger">CANCEL</Button>
-      <Button clicked={props.purchaseContinued} btnType="Success">CONTINUE</Button>
+      <Button clicked={this.props.purchaseCanceled} btnType="Danger">CANCEL</Button>
+      <Button clicked={this.props.purchaseContinued} btnType="Success">CONTINUE</Button>
     </Aux>
-  )
-};
-
-export default orderSummary;
+    );
+  }
+}
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,20 +3,33 @@ import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-      >
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends React.Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate() {
+    console.log('[Modal] will update')
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}
+          >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 /**
  * Doing inline stylings to handle dynamic display
  * of the modal. translateY lets it slide in and out.
@@ -24,4 +37,4 @@ const modal = (props) => (
  * opacity handles the actual changing of the visiblity
  */
 
-export default modal;
+export default Modal;

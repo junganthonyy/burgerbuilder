66306ce939e4c146394441797e8ca79da3702f36
Anthony Jung <junganthonyy@gmail.com>
2020-05-31 23:42:32 -0700

    Fixing Rules for Dropdown

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -86,6 +86,7 @@ export default class ContactData extends Component {
           ]
         },
         value: '',
+        validation: {},
         valid: true
       }
     },
@@ -125,10 +126,6 @@ export default class ContactData extends Component {
 
   checkValidity = (value, rules) => {
     let isValid = true;
-
-    if (!rules) {
-      return true;
-    }
 
     if (rules.required) {
       isValid = value.trim() !== '' && isValid;

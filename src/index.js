769b96285996ae7398fd8055851ea9e83e09e632
Anthony Jung <junganthonyy@gmail.com>
2020-06-04 16:23:37 -0700

    Changing File Stucture for Redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducer from '../src/store/reducer';
+import reducer from './store/reducers/burgerBuilder';
 
 import './index.css';
 import App from './App';

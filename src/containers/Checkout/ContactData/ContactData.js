5dd749434be7dd4aeadfac9038d0e297289704c1
Anthony Jung <junganthonyy@gmail.com>
2020-06-04 17:55:09 -0700

    Connecting Contact Data to Redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -185,7 +185,7 @@ class ContactData extends Component {
       <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
     </form>);
 
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner></Spinner>
     }
 
@@ -201,13 +201,14 @@ class ContactData extends Component {
 const mapStateToProps = (state) => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
   }
 }
 

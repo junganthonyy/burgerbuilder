cfe613d192a67c27031e0fdf0761fe0dd985d81e
Anthony Jung <junganthonyy@gmail.com>
2020-06-15 16:22:11 -0700

    Converting OrderSummary To React Hooks

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,29 +4,28 @@ import React from 'react';
 import Button from '../../UI/Button/Button';
 import Aux from '../../../hoc/Aux';
 
-class OrderSummary extends React.Component {
-  render() {
-    const ingredientSummary = Object.keys(this.props.ingredients).map((el) => {
-      return (
-        <li key={el}>
-          <span style={{textTransform: 'capitalize'}}>{el}</span>: {this.props.ingredients[el]}
-        </li>
-      )
-    })
-
+const orderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map((el) => {
     return (
-      <Aux>
+      <li key={el}>
+        <span style={{textTransform: 'capitalize'}}>{el}</span>: {props.ingredients[el]}
+      </li>
+    )
+  })
+
+  return (
+    <Aux>
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>
         {ingredientSummary}
       </ul>
-      <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
       <p>Continue To Checkout?</p>
-      <Button clicked={this.props.purchaseCanceled} btnType="Danger">CANCEL</Button>
-      <Button clicked={this.props.purchaseContinued} btnType="Success">CONTINUE</Button>
+      <Button clicked={props.purchaseCanceled} btnType="Danger">CANCEL</Button>
+      <Button clicked={props.purchaseContinued} btnType="Success">CONTINUE</Button>
     </Aux>
-    );
-  }
+  );
+  
 }
-export default OrderSummary;
+export default orderSummary;
